bc24196bfc2f009f0d66bf2e701deb8354b69ea1
TLindauer <tiernanxklindauer@gmail.com>
2024-02-20 09:15:07 -0600

    load cad wor

--- load_cad.py
+++ load_cad.py
@@ -9,13 +9,15 @@ coll_mat = cno.ChMaterialSurfaceNSC()
 
 # ground object
 ground = cno.ChBodyEasyBox(1, 0.05, 1, 100, True, True, coll_mat)
-ground.SetPos(cno.ChVectorD(0, -1, 0))
+ground.SetPos(cno.ChVectorD(0, -0.25, 0))
 ground.SetBodyFixed(True)
+ground.SetMass(100)
 system.Add(ground)
 
 # Load cad model top.stl
 my_doc = cascade.ChCascadeDoc()
-# load_ok = my_doc.Load_STEP('C:/Users/tenant/PycharmProjects/chrono-sim/top.stp')
+
+# load_ok = my_doc.Load_STEP('C:/Users/tenant/PycharmProjects/chrono-sim/Part1.step')
 load_ok = my_doc.Load_STEP(cno.GetChronoDataFile('cascade/assembly.stp'))
 # cno.ChCollisionModel.SetDefaultSuggestedEnvelope(0.002)
 # cno.ChCollisionModel.SetDefaultSuggestedMargin(0.001)
@@ -23,18 +25,32 @@ load_ok = my_doc.Load_STEP(cno.GetChronoDataFile('cascade/assembly.stp'))
 body_1 = cno.ChBody()
 if load_ok:
     shape_1 = TopoDS_Shape()
-    if (my_doc.GetNamedShape(shape_1, "Assem1/body1")):
+    # part_name = "65d4bcf3b21ea63a4383f41e"
+    part_name = "Assem1/body1"
+    if my_doc.GetNamedShape(shape_1, part_name):  # Assem1/body1 is the name of the shape in the STEP file
         mbody_1 = cascade.ChCascadeBodyEasy(shape_1, 1000, True, True, coll_mat)
-        mbody_1.SetMass(1)
+
+        # Most of the time, you'd run something like the following to get it right side up
+        rot_1 = cno.Q_from_AngAxis(cno.CH_C_PI / 2, cno.ChVectorD(1, 0, 0))  # Rotate 90 degrees on X axis
+        rot_2 = cno.Q_from_AngAxis(cno.CH_C_PI, cno.ChVectorD(0, 1, 0))  # Rotate 180 degrees on vertical Y axis
+        tot_rotation = rot_1 % rot_2  # This operator is just regular quaternion product
+
+        root_frame = cno.ChFrameMovingD(cno.ChVectorD(0, 0, 0), tot_rotation)
+        mbody_1.ConcatenatePreTransformation(root_frame)
+
+        mbody_1.SetMass(1)  # Setting the mass to 0 would mean no gravity
         print("Loaded assembly.")
         mbody_1.SetPos(cno.ChVectorD(0, 0, 0))
-        mbody_1.SetPos_dt(cno.ChVectorD(0, 0.05, 0))
+        mbody_1.SetPos_dt(cno.ChVectorD(0, 1, 0))
+        mbody_1.SetRot_dt(cno.Q_from_AngAxis(cno.CH_C_PI / 4, cno.ChVectorD(0, 0, 7)))
+        # For above:
+        # First argument sets velocity to flip it vertically, second rotates about the hole axis, third along the other
 
 
         system.Add(mbody_1)
 
     else:
-        print("Error: cannot retrieve the shape named automotive_design")
+        raise RuntimeError(f"Error: cannot retrieve the shape named {part_name}")
 else:
     print("Error: cannot load the file.")
 
@@ -42,7 +58,7 @@ else:
 # Create the Irrlicht visualization
 vis = irr.ChVisualSystemIrrlicht()
 vis.AttachSystem(system)
-vis.SetWindowSize(1024, 768)
+# vis.SetWindowSize(1024, 768)
 vis.SetWindowTitle('CAD demo')
 vis.Initialize()
 vis.AddLogo(cno.GetChronoDataFile('logo_pychrono_alpha.png'))
