159625bcb67493adf5cb9dddd570ca3b2269c9ae
TLindauer <tiernanxklindauer@gmail.com>
2024-02-19 21:12:59 -0600

    demo stuff here

--- rigid_bodies.py
+++ rigid_bodies.py
@@ -6,34 +6,54 @@ my_system = chrono.ChSystemSMC()
 
 # The collision system to use. "Bullet" is just the name of the library used
 my_system.SetCollisionSystem(chrono.ChCollisionSystemBullet())
-
-body_b = chrono.ChBody()
-body_b.SetMass(10)
-body_b.SetInertiaXX(chrono.ChVectorD(4, 4, 4))
-body_b.SetPos(chrono.ChVectorD(0.2, 0.4, 2.0))
-body_b.SetPos_dt(chrono.ChVectorD(0.1, 0.0, 0.0))
-body_b.SetCollide(True)
-
 coll_mat = chrono.ChMaterialSurfaceSMC()
 
-sphere_body = chrono.ChBodyEasySphere(
-    0.5,
-    1000,
+# box for the ground, it'll be fixed so the ball can bounce on it
+ground = chrono.ChBodyEasyBox(
+    10, 0.1, 10,
+    100,  # Density
     True,  # Visualization
     True,  # Collision
     coll_mat
 )
+ground.SetBodyFixed(True)
+ground.SetPos(chrono.ChVectorD(0, -1, 0))
+my_system.AddBody(ground)
 
+
+sphere_body = chrono.ChBodyEasySphere(
+    0.5,
+    100,
+    True,  # Visualization
+    True,  # Collision
+    coll_mat
+)
+sphere_body.SetMass(10)
 sphere_body.SetPos(chrono.ChVectorD(0, 0, 0))
-sphere_body.SetPos_dt(chrono.ChVectorD(0, 0, 0))
+sphere_body.SetPos_dt(chrono.ChVectorD(1, 10, 0))  # This gives the ball an initial upward velocity of 10
 my_system.AddBody(sphere_body)
+
+
+cylindrical_body = chrono.ChBodyEasyCylinder(
+    chrono.ChAxis_Y,
+    0.125,
+    3.0,
+    100,
+    True,  # Visualization
+    True,  # Collision
+    coll_mat
+)
+cylindrical_body.SetMass(0)
+cylindrical_body.SetPos(chrono.ChVectorD(3, 3, 0))
+cylindrical_body.SetPos_dt(chrono.ChVectorD(1, 1, 0))
+cylindrical_body.SetRot_dt(chrono.Q_from_AngAxis(chrono.CH_C_PI / 4, chrono.ChVectorD(1, 1, 0)))
+my_system.AddBody(cylindrical_body)
+
 # Create the Irrlicht visualization
-
-
 vis = chronoirr.ChVisualSystemIrrlicht()
 vis.AttachSystem(my_system)
 vis.SetWindowSize(1024, 768)
-vis.SetWindowTitle('Collision visualization demo')
+vis.SetWindowTitle('Rigid Body Demo')
 vis.Initialize()
 vis.AddLogo(chrono.GetChronoDataFile('logo_pychrono_alpha.png'))
 vis.AddSkyBox()
@@ -41,24 +61,8 @@ vis.AddCamera(chrono.ChVectorD(0, 8, 6))
 vis.AddTypicalLights()
 
 
-class DebugDrawer(chrono.VisualizationCallback):
-    def __init__(self):
-        super().__init__()
-
-
-    def DrawLine(self, pA, pB, color):
-        print("   pA = ", pA.x, pA.y, pA.z)
-        print("   pB = ", pB.x, pB.y, pB.z)
-
-
-# Create collision shape drawer
-drawer = DebugDrawer()
-my_system.GetCollisionSystem().RegisterVisualizationCallback(drawer)
-
 # Specify what information is visualized
 mode = chrono.ChCollisionSystem.VIS_Shapes
-
-use_zbuffer = True
 
 #  Run the simulation
 while vis.Run():
@@ -67,5 +71,4 @@ while vis.Run():
     vis.EndScene()
     my_system.DoStepDynamics(1e-3)
 
-    # print(sys.GetChTime(), "  ", sys.GetNcontacts())
     my_system.GetCollisionSystem().Visualize(chrono.ChCollisionSystem.VIS_Shapes)
